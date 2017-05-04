0427c4b965e4a1bc13b6fde216d8ee960c1f077e
vanessavj <vanessa.vj@web.de>
2017-05-04 15:56:39 +0200

    add edit text, shared preferences. new area, centroid

--- app/src/main/java/com/example/linux/polygonmaps/MapsActivity.java
+++ app/src/main/java/com/example/linux/polygonmaps/MapsActivity.java
@@ -4,8 +4,11 @@ import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -21,20 +24,21 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-import static java.lang.Math.acos;
-import static java.lang.Math.cos;
-import static java.lang.Math.sin;
+import com.google.maps.android.SphericalUtil;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     SharedPreferences settings;
+    SharedPreferences.Editor editor;
+    private EditText editText;
     private Button buttonStartStop;
     private Button buttonClear;
     private boolean drawLines;
     private PolylineOptions polylineOptions;
     private PolygonOptions polygon;
     private Polyline polyline;
+    private String polygonMarkers = "polygonMarkers";
 
 
     @Override
@@ -46,13 +50,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
+        System.out.println("Hallo");
+
         // Restore preferences
         settings = getPreferences(MODE_PRIVATE);
+        editor = settings.edit();
+
+
+
+        editText = (EditText) findViewById(R.id.editText);
         buttonClear = (Button) findViewById(R.id.clear);
         buttonClear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 mMap.clear();
+                editor.clear();
+                editor.apply();
             }
         });
         buttonStartStop = (Button) findViewById(R.id.StartStopPolygon);
@@ -63,9 +76,12 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     if(polylineOptions.getPoints().size() > 1){
                         // Draw polygon
                         polyline.remove();
-                        mMap.addPolygon(polygon.strokeColor(Color.RED).fillColor(Color.RED));
-                        double area = calcArea();
-                        LatLng center = calcCentroid(area);
+                        mMap.addPolygon(polygon.strokeColor(Color.RED).fillColor(0x4FFF0000));
+
+                        //Area Berechnung: http://stackoverflow.com/questions/28838287/calculate-the-area-of-a-polygon-drawn-on-google-maps-in-an-android-application
+                        double area = SphericalUtil.computeArea(polylineOptions.getPoints());
+
+                        LatLng center = calcCentroid();
                         mMap.addMarker(new MarkerOptions().position(center).title(area + ""));
                     }
                     buttonStartStop.setText("Start Polygon");
@@ -74,7 +90,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                     drawLines = false;
                 }else{
                     // Start Drawing Lines
-                    buttonStartStop.setText("Stop Polygon");
+                    buttonStartStop.setText("End Polygon");
                     drawLines = true;
                     polylineOptions = new PolylineOptions();
                     polygon = new PolygonOptions();
@@ -87,80 +103,78 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
     }
 
-    private LatLng calcCentroid(double area) {
+    //https://en.wikipedia.org/wiki/Centroid
+    private LatLng calcCentroid() {
         List<LatLng> points = polygon.getPoints();
-        double lat = 0;
-        double lng = 0;
-        double x,x2,y,y2 = 0;
 
-        for (int i = 0; i < points.size()-1; i++){
-            x = convert_x(points.get(i));
-            x2 = convert_x(points.get(i + 1));
-            y = convert_y(points.get(i));
-            y2 = convert_y(points.get(i + 1));
-            lat += (x + x2) * (x * y2 - x2 * y);
-            lng += (y + y2) * (x * y2 - x2 * y);
+        double x = 0, y = 0;
+
+        for (int i = 0; i < points.size(); i++) {
+            x += points.get(i).latitude;
+            y += points.get(i).longitude;
         }
-        double fac = 1 /(6 * area);
-        lat *= fac;
-        lng *= fac;
 
-        //double lat2 = acos((lng) / (6371000 * sin(lng)));
-
-        return new LatLng(lat, lng);
+        x /= points.size();
+        y /= points.size();
+        return new LatLng(x,y);
     }
+
+    /*Ursprüngliche Implementierung nach der Wikipedia Formel:
+     https://de.wikipedia.org/wiki/Polygon
+      hat leider nicht funktioniert
 
     private double calcArea() {
         List<LatLng> points = polygon.getPoints();
         double area = 0;
         double x,x2,y,y2 = 0;
-        double R = 6371000;
 
-        for (int i = 0; i < points.size()-1; i++){
+
+        for (int i = 0; i < points.size() - 1; i++){
             x = convert_x(points.get(i));
             x2 = convert_x(points.get(i + 1));
             y = convert_y(points.get(i));
             y2 = convert_y(points.get(i + 1));
             area += (x * y2) - (x2 * y);
         }
-        return area * 0.5;
+        return Math.abs(area) * 0.5;
     }
 
+    // http://stackoverflow.com/questions/1185408/converting-from-longitude-latitude-to-cartesian-coordinates
     private double convert_x(LatLng latLng) {
-        return (6371000 * cos(latLng.latitude) * cos(latLng.longitude));
+        return (6371 * cos(latLng.latitude) * cos(latLng.longitude));
     }
 
     private double convert_y(LatLng latLng) {
-        return (6371000 * cos(latLng.latitude) * sin(latLng.longitude));
-    }
+        return (6371 * cos(latLng.latitude) * sin(latLng.longitude));
+    } */
 
-
-
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
+        if(settings.contains(polygonMarkers)){
+            Set<String> markers = settings.getStringSet(polygonMarkers, new HashSet<String>());
+            for (String marker : markers){
+                Log.i("Marker", marker);
+                String[] tmp = marker.split(",");
+                mMap.addMarker(new MarkerOptions().position(new LatLng(Double.parseDouble(tmp[1]), Double.parseDouble(tmp[2]))).title(tmp[0]));
+            }
+
+        }
         // Add a marker in Sydney and move the camera
         LatLng weimar = new LatLng(50.980557, 11.332340);
         mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
             @Override
             public void onMapLongClick(LatLng latLng) {
-                mMap.addMarker(new MarkerOptions().position(latLng).title("Schloss Weimar"));
-                Set<String> set = settings.getStringSet("set", new HashSet<String>());
-                set.add(latLng.toString());
-                SharedPreferences.Editor editor = settings.edit();
-                editor.putStringSet("set", set);
-                // Commit the edits!
-                editor.commit();
+                String title = editText.getText().toString();
+                MarkerOptions markerOptions = new MarkerOptions().position(latLng).title(title);
+                mMap.addMarker(markerOptions);
+                Set<String> set = settings.getStringSet(polygonMarkers, new HashSet<String>());
+                Log.i("Set", set.toString());
+                set.add(markerOptions.getTitle() + "," + markerOptions.getPosition().latitude + "," + markerOptions.getPosition().longitude);
+                editor.clear();
+                editor.putStringSet(polygonMarkers, set);
+                editor.apply();
                 if(drawLines){
                     polygon.add(latLng);
                     if (polyline != null){
@@ -174,7 +188,6 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             }
         });
 
-        //mMap.addMarker(new MarkerOptions().position(weimar).title("Schloss Weimar"));
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(weimar, 18));
 
 
